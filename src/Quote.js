a57972a7063e3685acb3aeeedbb498268cef314f
Krushinsky <tj_chel@mail.ru>
2022-11-27 21:08:55 +0500

    fix link API

--- src/Quote.js
+++ src/Quote.js
@@ -18,7 +18,7 @@ function Qouote() {
     newQoutes()
     }, [])
     const newQoutes = async () =>{
-    const response = await fetch('http://www.boredapi.com/api/activity/ ')
+    const response = await fetch('https://www.boredapi.com/api/activity/ ')
     const data = await response.json()
     console.log(response)
     console.log(data)
